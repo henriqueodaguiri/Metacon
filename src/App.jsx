6b8c706281975f1830c26838f4048036b68d372d
MessiasGui <119057255+MessiasGui@users.noreply.github.com>
2024-07-14 20:38:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Button } from 'primereact/button';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.cs'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
